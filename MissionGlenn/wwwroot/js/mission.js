e3219fe2601b0b57228d4aa39a084e0057f4bd81
glennlivi9 <82484261+glennlivi9@users.noreply.github.com>
2022-01-19 22:00:43 -0700

    Initial commit

---
+++ MissionGlenn/wwwroot/js/mission.js
@@ -0,0 +1,57 @@
+﻿$("#calculator").click(function () {
+    var iAssignWeight = .55;
+    var iGroupWeight = .05;
+    var iQuizWeight = .1;
+    var iExamWeight = .2;
+    var iIntexWeight = .1;
+    var letterGrade = "";
+
+    //bring in input values
+
+    var iAssignPct = iAssignWeight * $("#assign").val();
+    var iGroupPct = iGroupWeight * $("#group").val();
+    var iQuizPct = iQuizWeight * $("#quiz").val();
+    var iExamPct = iExamWeight * $("#exam").val();
+    var iIntexPct = iIntexWeight * $("#intex").val();
+
+    var totalPct = ((iAssignPct + iGroupPct + iQuizPct + iExamPct + iIntexPct))
+
+    if (totalPct > 94) {
+        letterGrade = "A"
+    }
+    else if (totalPct > 90) {
+        letterGrade = "A-"
+    }
+    else if (totalPct > 87) {
+        letterGrade = "B+"
+    }
+    else if (totalPct > 83) {
+        letterGrade = "B"
+    }
+    else if (totalPct > 80) {
+        letterGrade = "B-"
+    }
+    else if (totalPct > 77) {
+        letterGrade = "C+"
+    }
+    else if (totalPct > 73) {
+        letterGrade = "C"
+    }
+    else if (totalPct > 70) {
+        letterGrade = "C-"
+    }
+    else if (totalPct > 67) {
+        letterGrade = "D+"
+    }
+    else if (totalPct > 63) {
+        letterGrade = "D"
+    } else if (totalPct > 60) {
+        letterGrade = "D-"
+    }
+    else {
+        letterGrade = "F"
+    }
+
+    alert("Your final grade is " + totalPct + "% with a letter grade of " + letterGrade )
+
+})
